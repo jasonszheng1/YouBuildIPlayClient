fdfcc934828f5fded55f3a62f0c62628bd70f505
jasonszheng1 <573583370@qq.com>
2022-10-28 21:19:07 +0800

    add a empty project

---
+++ js/main.js
@@ -0,0 +1,52 @@
+import {intMath, fraction} from "./game/intmath"
+
+let ctx = canvas.getContext('2d')
+let udp = wx.createUDPSocket()
+let port = udp.bind()
+udp.send({
+  address:'127.0.0.1',
+  port:30000,
+  message:'hello nihaoasfaaaa'
+})
+
+
+wx.onSocketOpen((result) => {
+  console.log('onSocketOpen', result)
+})
+
+wx.onSocketClose((result) => {
+  console.log('onSocketClose', result)
+})
+
+wx.onSocketError((result) => { 
+  console.log('onSocketError', result)
+})
+/**
+ * 游戏主函数 
+ */
+export default class Main {
+  constructor() {
+    this.bindTick     = this.tick.bind(this)
+    this.aniId = window.requestAnimationFrame(this.bindTick, canvas)
+    this.lastTickTime = Date.now()
+  }
+
+  // 实现游戏帧循环
+  tick() {
+    // get delta time
+    let now = Date.now()
+    let dt = (now - this.lastTickTime) * 0.001
+    this.lastTickTime = now
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    let img     = new Image()
+    img.src = 'images/bullet.png'
+    ctx.drawImage(img, 10, 10, 100, 100)
+    
+    this.aniId = window.requestAnimationFrame(
+      this.bindTick,
+      canvas
+    )
+  }
+}
